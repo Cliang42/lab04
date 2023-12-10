85f8942cb4a281f0f8b38e27b88136844f777228
cliang42 <cliang0911@163.com>
2023-12-11 01:53:34 +0800

    update test

--- src/test/java/LargestNumberTest.java
+++ src/test/java/LargestNumberTest.java
@@ -12,18 +12,21 @@ public class LargestNumberTest {
         // Test case 1
         int[] nums1 = {10, 2};
         String expected1 = "210";
+        String expected20 = "210";
         String result1 = largestNumber.largestNumber(nums1);
         assertEquals(expected1, result1);
 
         // Test case 2
         int[] nums2 = {3, 30, 34, 5, 9};
         String expected2 = "9534330";
+
         String result2 = largestNumber.largestNumber(nums2);
         assertEquals(expected2, result2);
 
         // Test case 3
         int[] nums3 = {1};
         String expected3 = "1";
+
         String result3 = largestNumber.largestNumber(nums3);
         assertEquals(expected3, result3);
 
